f64987d03c93e4d01e75c0746e184b7eac38d959
compadre <zvvlad2@gmail.com>
2024-02-22 17:35:57 +0600

    VE: add animation for expanded state bottomSheet by offset

--- app-android/src/main/kotlin/com/example/myapplication/android/MainActivity.kt
+++ app-android/src/main/kotlin/com/example/myapplication/android/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
 import com.arkivanov.decompose.defaultComponentContext
 import com.example.myapplication.root.RootContent
 import com.example.myapplication.shared.root.DefaultRootComponent
@@ -11,6 +12,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         val root = DefaultRootComponent(componentContext = defaultComponentContext())
 
--- compose-ui/src/commonMain/kotlin/com/example/myapplication/edit/EditContent.kt
+++ compose-ui/src/commonMain/kotlin/com/example/myapplication/edit/EditContent.kt
@@ -2,46 +2,85 @@ package com.example.myapplication.edit
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import com.arkivanov.decompose.extensions.compose.jetbrains.subscribeAsState
 import com.example.myapplication.common.views.TextBold14
 import com.example.myapplication.common.views.TextMedium14
 import com.example.myapplication.shared.editComponent.EditComponent
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BottomEditContent(component: EditComponent) {
     val model by component.model.subscribeAsState()
 
-    val sheetState = rememberModalBottomSheetState()
+    val sheetState: SheetState = rememberModalBottomSheetState()
+    var expandedProgress by remember { mutableStateOf(0f) }
+    LaunchedEffect(Unit) {
+        snapshotFlow {
+            // Обернул, т.к. несмотря на LaunchedEffect, ломается для descktop'a
+            runCatching { sheetState.requireOffset() }.getOrDefault(0f)
+        }.map { currentOffset ->
+            when {
+                currentOffset <= 0 -> 1f
+                currentOffset <=50 -> 1 - currentOffset / 50
+                else -> 0f
+            }
+        }.distinctUntilChanged()
+        .collect {
+            expandedProgress = it
+        }
+    }
+
+    val roundedShapeValue by remember(expandedProgress) { mutableStateOf(30 * (1-expandedProgress)) }
     ModalBottomSheet(
+        modifier = Modifier.fillMaxSize(),
         onDismissRequest = {
             component.onBackClicked()
         },
-        sheetState = sheetState
+        sheetState = sheetState,
+        shape = RoundedCornerShape(
+            topStart = roundedShapeValue,
+            topEnd = roundedShapeValue,
+        ),
+        dragHandle = null,
     ) {
-        Text(model.greetingText)
         Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize(),
         ) {
-            Button(
-                onClick = { component.onUpdateGreetingText() },
+            Text(modifier = Modifier.alpha(expandedProgress), text = "Test text")
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
             ) {
                 Text(model.greetingText)
+
+                Button(
+                    onClick = { component.onUpdateGreetingText() },
+                ) {
+                    Text(model.greetingText)
+                }
+                TextMedium14(text = "Medium 14")
+                TextBold14(text = "Bold 14")
             }
-            TextMedium14(text = "Medium 14")
-            TextBold14(text = "Bold 14")
         }
     }
 }
