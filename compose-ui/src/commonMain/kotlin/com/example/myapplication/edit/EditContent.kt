e89ea471b8177fa9d694276a777cf64c3643294b
compadre <zvvlad2@gmail.com>
2024-02-27 16:00:23 +0600

    VE: add animation for top bottomSheet in expand state

--- compose-ui/src/commonMain/kotlin/com/example/myapplication/edit/EditContent.kt
+++ compose-ui/src/commonMain/kotlin/com/example/myapplication/edit/EditContent.kt
@@ -1,10 +1,17 @@
 package com.example.myapplication.edit
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
@@ -15,17 +22,23 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.jetbrains.subscribeAsState
+import com.example.myapplication.common.resources.icons.AppIcons
+import com.example.myapplication.common.resources.icons.painter
 import com.example.myapplication.common.views.TextBold14
 import com.example.myapplication.common.views.TextMedium14
 import com.example.myapplication.shared.editComponent.EditComponent
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -49,6 +62,14 @@ fun BottomEditContent(component: EditComponent) {
             expandedProgress = it
         }
     }
+    val scope = rememberCoroutineScope()
+    val animateToDismiss: () -> Unit = {
+        scope.launch { sheetState.hide() }.invokeOnCompletion {
+            if (!sheetState.isVisible) {
+                component.onBackClicked()
+            }
+        }
+    }
 
     val roundedShapeValue by remember(expandedProgress) { mutableStateOf(30 * (1-expandedProgress)) }
     ModalBottomSheet(
@@ -66,7 +87,20 @@ fun BottomEditContent(component: EditComponent) {
         Column(
             modifier = Modifier.fillMaxSize(),
         ) {
-            Text(modifier = Modifier.alpha(expandedProgress), text = "Test text")
+            Box(modifier = Modifier.fillMaxWidth()) {
+                Box(
+                    modifier = Modifier.padding(vertical = 16.dp).fillMaxWidth().alpha(1-expandedProgress),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    DragHandleView()
+                }
+
+                Icon(
+                    modifier = Modifier.padding(16.dp).alpha(expandedProgress).clickable { animateToDismiss() },
+                    painter = AppIcons.CloseIcon.painter(),
+                    contentDescription = null
+                )
+            }
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center,
@@ -83,4 +117,9 @@ fun BottomEditContent(component: EditComponent) {
             }
         }
     }
+}
+
+@Composable
+private fun DragHandleView() {
+    Box(modifier = Modifier.size(width = 44.dp, height = 2.dp).background(Color.Cyan))
 }
